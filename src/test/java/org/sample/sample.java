f01b1eab982fa79840eb8ee8084131e5b9fba399
arunprasath98 <arunmcekct1998@gmail.com>
2020-11-23 05:39:02 -0600

    arunproject

--- src/test/java/org/sample/sample.java
+++ src/test/java/org/sample/sample.java
@@ -4,8 +4,9 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
-
 @RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
+@CucumberOptions(features= {"src\\test\\resources"},glue= {"org.step"})
 @CucumberOptions(features= {"src\\test\\resources"},glue= {"org.step"})
 
 public class sample {
